bc502d9f69cdd79995563d8ab1920a02b257c688
Dmitry <krdv13@yandex.ru>
2024-08-30 14:56:05 +0700

    Домашняя работа Исключения в программировании и их обработка с изменнениями

---
+++ InvalidNumberOfArgumentsException.java
@@ -0,0 +1,8 @@
+package IskSem3DZ;
+
+public class InvalidNumberOfArgumentsException extends Exception {
+    public InvalidNumberOfArgumentsException(String message) {
+        super(message);
+    }
+}
+
